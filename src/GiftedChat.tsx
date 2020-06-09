57d599eade970c67d82ddf16e2e0c031058c6708
Alex Aphonin <alex@connecteam.com>
2020-06-09 22:38:35 +0300

    removed logging

--- src/GiftedChat.tsx
+++ src/GiftedChat.tsx
@@ -558,8 +558,6 @@ class GiftedChat<TMessage extends IMessage = IMessage> extends React.Component<
   }
 
   getMinInputToolbarHeight() {
-    console.log(`getMinInputToolbarHeight ${this.props.upperAccessoryHeight!}`)
-
     const minHeight = this.props.minInputToolbarHeight! + this.props.upperAccessoryHeight!
 
     return this.props.renderAccessory ? minHeight * 2 : minHeight
@@ -775,7 +773,6 @@ class GiftedChat<TMessage extends IMessage = IMessage> extends React.Component<
     if (layout.height <= 0) {
       return
     }
-    console.log('onInitialLayoutViewLayout')
 
     this.notifyInputTextReset()
     this.setMaxHeight(layout.height)
@@ -793,16 +790,12 @@ class GiftedChat<TMessage extends IMessage = IMessage> extends React.Component<
   }
 
   onMainViewLayout = (e: any) => {
-    console.log('onMainViewLayout')
-
     // fix an issue when keyboard is dismissing during the initialization
     const { layout } = e.nativeEvent
     if (
       this.getMaxHeight() !== layout.height ||
       this.getIsFirstLayout() === true
     ) {
-
-      console.log('onMainViewLayout: doLogic')
 
       this.setMaxHeight(layout.height)
       this.setState({
